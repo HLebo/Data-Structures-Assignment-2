9df58138e34e8ca5ea806a6db353f2ba301f68f4
HLebo <hjl7@zips.uakron.edu>
2016-10-02 16:42:20 -0400

    Added case for if user provides row of 1 element

--- main.cpp
+++ main.cpp
@@ -4,9 +4,27 @@ using namespace std;
 
 string lights(string row, int n, string &solution){
 
-    solution = "Testing";
+    if(n == 1){
+        if(row[0] == '1'){
+            row[0] = '0';
+            solution = "1";
+        }
+        return solution;
+    }
 
-    return "";
+    else if(n == 2){
+        return "";
+    }
+
+    else if(n == 3){
+        return "";
+    }
+
+    else{
+
+        return "";
+    }
+
 }
 
 int main()
@@ -15,15 +33,15 @@ int main()
     int rowLength;
     string output;
 
-    cout << "Please enter the row length: " << endl;
+    cout << "Please enter the row length: ";
     cin >> rowLength;
 
-    cout << "Please enter the light puzzle: " << endl;
+    cout << "Please enter the light puzzle: ";
     cin >> userRow;
 
     lights(userRow, rowLength, output);
 
-    cout << output;
+    cout << "The solution is " << output << endl;
 
     return 0;
 }
