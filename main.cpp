7b846b1f03da911578513268e649b0dae16e7f28
HLebo <hjl7@zips.uakron.edu>
2016-10-03 20:02:16 -0400

    Added comments, got majority of recursion cases working

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//changing the value of a character from 0 to 1 or 1 to 0
 char switchValue(char c){
     if(c == '0'){
         return '1';
@@ -12,8 +13,10 @@ char switchValue(char c){
         return '0';
 }
 
+//recursive lights function
 string lights(string row, int n, string &solution){
 
+    //base cases
     if(row.size() == 1){
         if(row[0] == '1'){
             row[0] = '0';
@@ -29,9 +32,13 @@ string lights(string row, int n, string &solution){
         if(row == "00"){
             solution = "Already solved";
         }
-        else if(row == "11"){
-            row = "00";
-            solution = "1";
+        else if(row[1] == '1'){
+            row[0] = switchValue(row[0]);
+            row[1] = switchValue(row[1]);
+
+            std::ostringstream oss;
+            oss << n;
+            solution = oss.str();
         }
         else{
             solution = "NO SOLUTION";
@@ -40,11 +47,21 @@ string lights(string row, int n, string &solution){
     }
 
     else if(row.size() == 3){
+
+        /*if(n == 4){
+            string subRow = row.substr(1,row.size());
+            string row_prev = lights(subRow, n, solution);
+            row_prev = row[0] + row_prev;
+        }
+
+        else*/
         if(row[2] == '1'){
+            //hitting the middle light
             row[0] = switchValue(row[0]);
             row[1] = switchValue(row[1]);
             row[2] = switchValue(row[2]);
 
+            //recording the index of the one we hit
             int x = n-1;
             std::ostringstream oss;
             oss << x;
@@ -53,12 +70,39 @@ string lights(string row, int n, string &solution){
         return row;
     }
 
-    else{
-        string subRow = row.substr(1,row.size());
-        string row_prev = lights(subRow, n, solution);
-        row_prev = row[0] + row_prev;
 
-        return row_prev;
+    else{
+        //removing first element from row to create smaller row
+        string subRow = row.substr(1,row.size());
+
+        //recursion
+        subRow = lights(subRow, n, solution);
+
+        //reconstructing the row
+        string newRow = row[0] + subRow;
+
+        if(newRow[2] == '1'){
+            newRow[0] = switchValue(newRow[0]);
+            newRow[1] = switchValue(newRow[1]);
+            newRow[2] = switchValue(newRow[2]);
+
+            //recording the index of the one we hit
+            int x = n - newRow.size() + 2;
+            std::ostringstream oss;
+            oss << x;
+            solution = oss.str() + ' ' + solution;
+        }
+
+        if(n == newRow.size()){
+            if(newRow[1] == '1'){
+                newRow[0] = switchValue(newRow[0]);
+                newRow[1] = switchValue(newRow[1]);
+
+                solution = "1 " + solution;
+            }
+        }
+
+        return newRow;
     }
 
 }
@@ -72,15 +116,20 @@ int main()
 
     do{
 
+    //asking user for input
     cout << "Please enter the row length: ";
     cin >> rowLength;
 
     cout << "Please enter the light puzzle: ";
     cin >> userRow;
 
+    //ensure output is empty before calling recursive function
+    output = "";
+
+    //printing output
     cout << "Ending row " << lights(userRow, rowLength, output) << endl;
 
-    cout << "The solution is " << output << endl;
+    cout << "The solution is " << output << endl << endl;
 
     } while(rowLength != 0);
 
